703bef1d0ed1fdb53fc71ed58c72fb90273666d1
Гребенец Ден < >
2017-04-21 11:23:53 +0300

    Provide simple grid sysytem

--- react-search/src/js/components/Form.js
+++ react-search/src/js/components/Form.js
@@ -6,15 +6,7 @@ export default class Form extends Component {
         <form id="searchForm">
             <fieldset>
                 <input onChange={ this.props.handleChange } id="s" type="text" />
-
                 <input onClick={ this.props.handleSearch } readOnly value="Submit" id="submitButton" />
-
-                <ul className="icons">
-                    <li className="web" title="Web Search" data-searchType="web">Web</li>
-                    <li className="images" title="Image Search" data-searchType="images">Images</li>
-                    <li className="news" title="News Search" data-searchType="news">News</li>
-                    <li className="videos" title="Video Search" data-searchType="video">Videos</li>
-                </ul>
             </fieldset>
         </form>
     );
--- react-search/src/js/components/Result.js
+++ react-search/src/js/components/Result.js
@@ -6,20 +6,22 @@ export default class Result extends Component {
   renderResult() {
     if(this.props.posts[0]) {
       return this.props.posts.map((post) => {
-        return <Link key={ Math.random() } to={ "/post/" + post.id }><h2 onClick={ () => {  } } className="post-title" key={ Math.random() }>{ post.title} </h2></Link>
+        return <Link className="result-link col-3" key={ Math.random() } to={ "/post/" + post.id }><h2 onClick={ () => {  } } className="post-title" key={ Math.random() }>{ post.title} </h2></Link>
       });
     }
     else {
       if(this.props.isSearched) {
-        return <h2>Such user doesn't have any posts.</h2>;
+        return <h2 className="fail-result">Such user doesn't have any posts.</h2>;
       }
     }
   }
   
   render() {
     return (
-      <div>
-        { this.renderResult() }
+      <div className="container">
+        <div className="row">
+          { this.renderResult() }
+        </div>
       </div>
     );
   }
